73c4c5067963f67f0ebbe21d764075c281f516fe
Theo Sim <theo.sim.dev@gmail.com>
2025-06-17 20:04:11 -0400

    Initialize a simple node application to verify issue #6846 on axios

---
+++ index.js
@@ -0,0 +1,33 @@
+// Verifying this issue: https://github.com/axios/axios/issues/6846
+const axios = require('axios');
+
+// 1. Using default axios(config)
+axios({
+  url: 'https://jsonplaceholder.typicode.com/todos/1',
+  method: 'get'
+})
+  .then(response => {
+    console.log('Default axios(config) result:');
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error('Error with axios(config):', error.message);
+  });
+
+// 2. Using instance(config)
+const instance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: { 'X-Test-Header': 'HelloWorld' }
+});
+
+instance({
+  url: '/todos/1',
+  method: 'get'
+})
+  .then(response => {
+    console.log('instance(config) result:');
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error('Error with instance(config):', error.message);
+  });
